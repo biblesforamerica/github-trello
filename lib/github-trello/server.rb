75fb0238f94095ce427dac32b6ba451a4d619f3a
Lisa Burricks <lisaburricks@gmail.com>
2014-08-01 13:57:06 -0700

    deployment changes

--- lib/github-trello/server.rb
+++ lib/github-trello/server.rb
@@ -34,56 +34,54 @@ module GithubTrello
         return
       end
 
-      puts "hello"
+      http = GithubTrello::HTTP.new(config["users"][committer]["oauth_token"], config["users"][committer]["api_key"])
 
-    #   http = GithubTrello::HTTP.new(config["users"][committer]["oauth_token"], config["users"][committer]["api_key"])
+      payload["commits"].each do |commit|
+        # Figure out the card short id
+        match = commit["message"].match(/((doing|review|done|archive)e?s? \D?([0-9]+))/i)
+        next unless match and match[3].to_i > 0
 
-    #   payload["commits"].each do |commit|
-    #     # Figure out the card short id
-    #     match = commit["message"].match(/((doing|review|done|archive)e?s? \D?([0-9]+))/i)
-    #     next unless match and match[3].to_i > 0
+        results = http.get_card(board_id, match[3].to_i)
+        unless results
+          puts "[ERROR] Cannot find card matching ID #{match[3]}"
+          next
+        end
 
-    #     results = http.get_card(board_id, match[3].to_i)
-    #     unless results
-    #       puts "[ERROR] Cannot find card matching ID #{match[3]}"
-    #       next
-    #     end
+        results = JSON.parse(results)
 
-    #     results = JSON.parse(results)
+        # Add the commit comment
+        message = "#{commit["message"]}\n\n[#{branch}] #{commit["url"]}"
+        # message = "hello"
+        message.gsub!(match[1], "")
+        message.gsub!(/\(\)$/, "")
 
-    #     # Add the commit comment
-    #     message = "#{commit["message"]}\n\n[#{branch}] #{commit["url"]}"
-    #     # message = "hello"
-    #     message.gsub!(match[1], "")
-    #     message.gsub!(/\(\)$/, "")
+        http.add_comment(results["id"], message)
 
-    #     http.add_comment(results["id"], message)
+        if match[2].downcase == "archive"
+          then to_update = {:closed => true}
+        else
+          to_update = {}
+          # Determine the action to take
+          move_to = case match[2].downcase
+            when "doing" then config["repos"][repo]["on_doing"]["move_to"]
+            when "review" then config["repos"][repo]["on_review"]["move_to"]
+            when "done" then config["repos"][repo]["on_done"]["move_to"]
+          end
 
-    #     if match[2].downcase == "archive"
-    #       then to_update = {:closed => true}
-    #     else
-    #       to_update = {}
-    #       # Determine the action to take
-    #       move_to = case match[2].downcase
-    #         when "doing" then config["repos"][repo]["on_doing"]["move_to"]
-    #         when "review" then config["repos"][repo]["on_review"]["move_to"]
-    #         when "done" then config["repos"][repo]["on_done"]["move_to"]
-    #       end
+           #move_to = update_config["move_to"]
 
-    #        #move_to = update_config["move_to"]
+          unless results["idList"] == move_to
+            to_update[:idList] = move_to
+          end
+        end
 
-    #       unless results["idList"] == move_to
-    #         to_update[:idList] = move_to
-    #       end
-    #     end
+        unless to_update.empty?
+          http.update_card(results["id"], to_update)
+        end
+      end
 
-    #     unless to_update.empty?
-    #       http.update_card(results["id"], to_update)
-    #     end
-    #   end
-
-    #   ""
-    # end
+      ""
+    end
 
     get '/' do
       "Hello World!"
