30669903b5e15476bae6d36753b215262a72e2e1
Lisa Burricks <lisaburricks@gmail.com>
2014-08-06 10:09:10 -0700

    comments cleaned, code organized

--- lib/github-trello/server.rb
+++ lib/github-trello/server.rb
@@ -9,71 +9,44 @@ require "yaml"
 module GithubTrello
   class Server < Sinatra::Base
     post "/posthook" do
-      #connect to database  teasing
+
+      #connect to database  
       pg = GithubTrello::Postgres.new
       pg.connect
+
+      #load data from payload
       payload = JSON.parse(params[:payload])
       committer = payload["head_commit"]["committer"]["username"]
-      # path = File.expand_path(File.dirname(__FILE__) + "/../../conf.yml")
-      # config = YAML::load(File.read(path))
       repo = payload["repository"]["name"]
-      unless pg.userTable[committer] #unless config["users"][committer]
+      unless pg.userTable[committer] 
         puts "[ERROR] Github username not recognized. Run rake add_user"
+        return
       end
-
-      unless pg.repoTable[repo] #unless config["repos"][repo]
+      unless pg.repoTable[repo] 
         puts "[ERROR] Github repo not recognized. Run rake add_repo"
+        return
       end
+      branch = payload["ref"].gsub("refs/heads/", "")
 
-      #deploy comment
-
-      board_id = pg.repoTable[repo]["board_id"] #config["repos"][repo]["board_id"]
+      #load board_id from the database
+      board_id = pg.repoTable[repo]["board_id"] 
       unless board_id
         puts "[ERROR] Commit from #{payload["repository"]["name"]} but no board_id entry found in config. Run rake update_repo"
         return
       end
 
-      branch = payload["ref"].gsub("refs/heads/", "")
-
-      # if config["blacklist_branches"] and config["blacklist_branches"].include?(branch)
-      #   return
-      # elsif config["whitelist_branches"] and !config["whitelist_branches"].include?(branch)
-      #   return
-      # end
-
-      # http = GithubTrello::HTTP.new(config["users"][committer]["oauth_token"], config["users"][committer]["api_key"])
-
+      #connect to Trello's server
       http = GithubTrello::HTTP.new(pg.userTable[committer]["oauth_token"], pg.userTable[committer]["api_key"])
 
-      # puts payload["commits"].inspect
-      # array = ["hello", "everybody", "praise", "the", "Lord!"]
-      # #puts array
-      # array.each do |a|
-      #   print " yay! "
-      #   print a
-      # end
-
+      #search each commit from the payload for a flag to make a comment on a Trello card
       commits = payload["commits"].each do |commit|
-      #commits.each do |commit|
-      # payload["commits"].each do |commit|
-        # Figure out the card short id toggle
-
-        #I THINK THE PROBLEM IS IN THE MATCH STATEMENT RIGHT HERE 
-
         match = commit["message"].match(/((card|doing|review|done|archive)e?s? \D?([0-9]+))/i)
         next unless match and match[3].to_i > 0
-        #   next
-        # end
-        # puts "hello"+match[3]
-
-        #get the cardsd
-        # results = http.get_card(board_id, 4)
         results = http.get_card(board_id, match[3].to_i)
         unless results
           puts "[ERROR] Cannot find card matching ID #{match[3]}"
           next
         end
-
         results = JSON.parse(results)
 
         # Add the commit comments
@@ -83,10 +56,11 @@ module GithubTrello
 
         http.add_comment(results["id"], message)
 
-         if match[2].downcase == "archive"
-           then to_update = {:closed => true}
-         else
-           to_update = {}
+        #move card if doing, review, done, or archive was flagged in commit message
+        if match[2].downcase == "archive"
+         then to_update = {:closed => true}
+        else
+          to_update = {}
           # Determine the action to take
           move_to = case match[2].downcase
             when "doing" then pg.repoTable[repo]["on_doing"] #config["repos"][repo]["on_doing"]
@@ -94,30 +68,25 @@ module GithubTrello
             when "done" then pg.repoTable[repo]["on_done"] #config["repos"][repo]["on_done"]
           end
 
+          #only move card if destination is not equal to origin
           unless results["idList"] == move_to
             to_update[:idList] = move_to
           end
 
-        unless to_update.empty?
-           http.update_card(results["id"], to_update)
-         end
-       end
+          #move card
+          unless to_update.empty?
+             http.update_card(results["id"], to_update)
+          end
+        end
 
        end
 
-      "" #line needed so that sinatra can happily return a string
+      "" #line needed so that sinatra can return a string
     end
 
     get '/' do
-      pg = GithubTrello::Postgres.new
-      pg.connect
-      pg.userTable.inspect
+      ""
     end
-
-    post '/' do
-      payload = JSON.parse(params[:payload])
-      puts payload
-    end
-
+    
   end
 end
