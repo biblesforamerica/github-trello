57537f0dda694bb2c6683c6cc1cba4afeddcb510
Lisa Burricks <lisaburricks@gmail.com>
2014-08-06 09:28:02 -0700

    4 test comment - next replaced with end in unless block

--- lib/github-trello/server.rb
+++ lib/github-trello/server.rb
@@ -61,14 +61,14 @@ module GithubTrello
         #I THINK THE PROBLEM IS IN THE MATCH STATEMENT RIGHT HERE 
 
         match = commit["message"].match(/((doing|review|done|archive)e?s? \D?([0-9]+))/i)
-        next unless match and match[3].to_i > 0
+        # next unless match and match[3].to_i > 0
 
         #get the card
-        results = http.get_card(board_id, 4)
-        # results = http.get_card(board_id, match[3].to_i)
+        # results = http.get_card(board_id, 4)
+        results = http.get_card(board_id, match[3].to_i)
         unless results
           puts "[ERROR] Cannot find card matching ID #{match[3]}"
-          next
+          end
         end
 
         results = JSON.parse(results)
