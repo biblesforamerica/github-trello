a856ca1f18de4196fc518d1a38af70990dd7ab0a
Lisa Burricks <lisaburricks@gmail.com>
2014-08-06 09:46:30 -0700

    4 test comment - gsub on messages added

--- lib/github-trello/server.rb
+++ lib/github-trello/server.rb
@@ -61,7 +61,9 @@ module GithubTrello
         #I THINK THE PROBLEM IS IN THE MATCH STATEMENT RIGHT HERE 
 
         match = commit["message"].match(/((doing|review|done|archive)e?s? \D?([0-9]+))/i)
-        next unless match and match[3].to_i > 0
+        unless match and match[3].to_i > 0
+          next
+        end
         # puts "hello"+match[3]
 
         #get the cardsd
@@ -76,8 +78,8 @@ module GithubTrello
 
         # Add the commit comments
         message = "#{commit["message"]}\n\n[#{branch}] #{commit["url"]}"
-        message.gsub!(match[1], "")
-        message.gsub!(/\(\)$/, "")
+        # message.gsub!(match[1], "")
+        # message.gsub!(/\(\)$/, "")
 
         http.add_comment(results["id"], message)
 
