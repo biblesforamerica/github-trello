5a60b29857b7b7e10fa5ff6d8756e2034ad0914a
Lisa Burricks <lisaburricks@gmail.com>
2014-08-06 09:45:09 -0700

    4 test comment - gsub on messages added

--- lib/github-trello/server.rb
+++ lib/github-trello/server.rb
@@ -61,7 +61,7 @@ module GithubTrello
         #I THINK THE PROBLEM IS IN THE MATCH STATEMENT RIGHT HERE 
 
         match = commit["message"].match(/((doing|review|done|archive)e?s? \D?([0-9]+))/i)
-        # next unless match and match[3].to_i > 0
+        next unless match and match[3].to_i > 0
         # puts "hello"+match[3]
 
         #get the cardsd
@@ -69,6 +69,7 @@ module GithubTrello
         results = http.get_card(board_id, match[3].to_i)
         unless results
           puts "[ERROR] Cannot find card matching ID #{match[3]}"
+          next
         end
 
         results = JSON.parse(results)
